a31dc4295d8c2cef7482e02cfe1a83f41a6ed1c1
Ksab070 <khansubhan070@gmail.com>
2025-09-01 23:33:29 +0530

    Fix responsiveness for skills section & in about-pic

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
+    const body = document.querySelector("body"); // Get the body element
 
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+    body.classList.toggle("no-scroll"); // Toggle the no-scroll class to prevent background scrolling
 }
