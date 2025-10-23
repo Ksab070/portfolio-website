8257457d59ff317c048bd8886d240d1e44d8ab6d
Subhan Khan <khansubhan070@gmail.com>
2025-10-23 23:44:19 +0530

    Complete s3, cloudfront

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -11,5 +11,8 @@ locals {
     frontend_files = fileset("${path.module}/../front-end", "**")
     
     s3_origin_id = "my-s3-origin"
+
+    zone_id = "31b75f303b467246315ff1d5fe271fef"
+    account_id = "1cdd8c65585d05850ca3bb2548e5c6e7"
 }
 
